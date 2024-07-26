6ce81fa7952ee2a081cc920cbac1d40031698ec7
arnau <arnaubago1993@gmail.com>
2024-07-26 11:06:31 +0200

    fix multiple keys resported at the same time

--- src/comm_external.c
+++ src/comm_external.c
@@ -9,6 +9,8 @@
 #include "task_internal.h"
 #include "usb_descriptors.h"
 
+#define KEY_REPORT_SIZE 6
+
 
 void comm_external_init(void)
 {
@@ -19,17 +21,31 @@ void comm_external_init(void)
 
 bool comm_external_key_received(uint8_t *key)
 {
-	if (pdTRUE == xQueueReceive(comm_external_queue, key, 0))
+	int key_report_index = 0;
+	int ret = false;
+
+	while (pdTRUE == xQueueReceive(comm_external_queue, &key[key_report_index], 0))
 	{
-		return true;
+		key_report_index++;
+		ret = true;
+		if (key_report_index >= KEY_REPORT_SIZE) 
+		{
+			// reached limit, return
+			break;
+		}
 	}
-	return false;
+
+	return ret;
 }
 
 bool comm_external_send_usb(uint8_t *key)
 {
 	uint8_t key_report[6] = {0};
-	key_report[0] = *key;
+	
+	for (int i = 0; i < KEY_REPORT_SIZE; ++i)
+	{
+		key_report[i] = key[i];
+	}
 
 	return tud_hid_keyboard_report(0, 0, key_report);
 }
@@ -45,12 +61,12 @@ void comm_external_task(void *pvParameters)
 	comm_external_init();
 	
 	TickType_t task_delay = TASK_DELAY_DEF;
-	bool usb_sent = false;	
-	uint8_t key;
+	bool usb_sent = false;
 
 	while(1)
 	{
 		tud_task();
+		uint8_t key_report[KEY_REPORT_SIZE] = {0};
 
 		if (tud_hid_ready()) {
 			if (usb_sent)
@@ -58,9 +74,9 @@ void comm_external_task(void *pvParameters)
 				comm_external_clear_usb_report();
 				usb_sent = false;
 			} 
-			else if (comm_external_key_received(&key))
+			else if (comm_external_key_received(&key_report[0]))
 			{
-				comm_external_send_usb(&key);
+				comm_external_send_usb(&key_report[0]);
 				usb_sent = true;
 				task_delay = TASK_DELAY_DEF;
 			}
