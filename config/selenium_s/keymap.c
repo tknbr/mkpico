d598e4068c5475bf43393b3b899989e45879afa3
arnau <arnaubago1993@gmail.com>
2024-08-13 06:26:37 +0200

    selenium s keymap updated

--- config/selenium_s/keymap.c
+++ config/selenium_s/keymap.c
@@ -25,21 +25,21 @@ uint8_t keymap[][NUM_ROWS][NUM_COLS] = {
     /*
      *
      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-     * │ESC│   │ UP│ [ │ ] │       │ 7 │ 8 │ 9 │ - │ * │
+     * │ESC│ ' │ UP│ [ │ ] │       │ 7 │ 8 │ 9 │ - │ / │
      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-     * │TAB│LFT│DWN│RGT│ # │       │ 4 │ 5 │ 6 │ = │ / │
+     * │TAB│LFT│DWN│RGT│ # │       │ 4 │ 5 │ 6 │ = │Ent│
      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-     * │Shf│   │ ; │ , │ . │       │ 1 │ 2 │ 3 │Alt│Shf│
+     * │Shf│ \ │ ; │ , │ . │       │ 1 │ 2 │ 3 │Alt│Shf│
      * └───┴───┴───┼───┼───┤       ├───┼───┼───┴───┴───┘
-     *             │   │LUp│       │ 0 │LDw│
+     *             │Bsp│LUp│       │ 0 │LDw│
      *             └───┴───┘       └───┴───┘ 
      *
      */
     [1] = LAYOUT_SELENIUM_S(
-       K_ESC,  K_NULL, K_A_U,  K_BRAL, K_BRAR,      K_7,    K_8,    K_9,    K_MIN, K_NULL,
-       K_TAB,  K_A_L,  K_A_D,  K_A_R,  K_HASH,      K_4,    K_5,    K_6,    K_EQU, K_SCLN,
-       K_SHFT, K_NULL, K_SCLN, K_COMM, K_DOT,       K_1,    K_2,    K_3,    K_ALT, K_SHFT,
-                               K_NULL, K_LYRUP,     K_0,    K_LYRDWN
+       K_ESC,  K_APOS, K_A_U,  K_BRAL, K_BRAR,      K_7,    K_8,    K_9,    K_MIN, K_SLSH,
+       K_TAB,  K_A_L,  K_A_D,  K_A_R,  K_HASH,      K_4,    K_5,    K_6,    K_EQU, K_ENT,
+       K_SHFT, K_BKSL, K_SCLN, K_COMM, K_DOT,       K_1,    K_2,    K_3,    K_ALT, K_SHFT,
+                               K_BSPC, K_LYRUP,     K_0,    K_LYRDWN
     )
 };
 
